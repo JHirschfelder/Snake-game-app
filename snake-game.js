ea33ea636fe25cfb302700579e9d702e8fca738b
James Hirschfelder <james.hirschfelder@gmail.com>
2021-06-03 16:22:39 -0700

    collision detection added

--- snake-game.js
+++ snake-game.js
@@ -3,21 +3,23 @@ let canvasContext;
 let headX = 200;
 let headY = 200;
 let snake = [ {x:headX, y:headY}];
-let dx=10;
+let dx=20;
 let dy=0;
+let hasCollided=false
 
 
 window.onload = function() {
   canvas=document.getElementById('game-canvas');
   canvasContext = canvas.getContext('2d');
   setInterval(function() {
-    console.log(headX);
     document.addEventListener("keydown", changeDirection)
     drawCanvas();
     moveSnake();
+    checkCollision();
+    console.log(hasCollided);
     drawApple();
     drawSnake();
-  },250);
+  },2000);
 };
 
 function drawCanvas() {
@@ -30,6 +32,22 @@ function moveSnake() {
   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
   snake.unshift(head);
   snake.pop();
+};
+
+function checkCollision() {
+  for( let snakepiece=1; snakepiece<snake.length; snakepiece++) {
+    if (snake[snakepiece].x == snake[0].x && 
+    snake[snakepiece].y == snake[0].y)
+      hasCollided=true;
+  };
+
+  if (
+    snake[0].x < 0 || 
+    snake[0].x >= canvas.width || 
+    snake[0].y < 0 || 
+    snake[0].y >= canvas.height
+    )
+    hasCollided=true;
 };
 
 function drawApple () {
@@ -58,16 +76,16 @@ function changeDirection(event)
   const pressKey = event.keyCode;
 
   switch (true) {
-    case (pressKey == leftArrow && dx!=10):
-      dx = -10; dy = 0; break;
+    case (pressKey == leftArrow && dx!=20):
+      dx = -20; dy = 0; break;
       
-    case (pressKey == upArrow && dy!=10):
-      dx = 0; dy = -10; break;
+    case (pressKey == upArrow && dy!=20):
+      dx = 0; dy = -20; break;
       
-    case (pressKey == rightArrow && dx!=-10):
-      dx = 10; dy = 0; break;
+    case (pressKey == rightArrow && dx!=-20):
+      dx = 20; dy = 0; break;
       
-    case (pressKey == downArrow && dy!=-10):
-      dx = 0; dy = 10; break;
+    case (pressKey == downArrow && dy!=-20):
+      dx = 0; dy = 20; break;
   }
 }
